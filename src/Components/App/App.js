105faed2bba98eeaaf6d60df721235c75e2f1698
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-06-03 23:12:58 -0400

    Added removedTrack method to remove songs from the playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,8 @@ class App extends React.Component {
     };
 
     this.addTrack = this.addTrack.bind(this);
+
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track){
@@ -25,6 +27,14 @@ class App extends React.Component {
     }
 
     tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+
+    tracks= tracks.filter(savedTrack => savedTrack.id !== track.id);
+
     this.setState({playlistTracks: tracks});
   }
   
@@ -42,6 +52,7 @@ class App extends React.Component {
             <Playlist 
               playlistName={this.state.playlistName} 
               playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
             />
           </div>
         </div>
